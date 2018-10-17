6e1bda381be648877f7897375e45ca32cfc90bce
SamuelDuke <SamuelWinterton@gmail.com>
2018-10-17 12:07:46 -0600

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: 8080,
-    //database:'mongodb://admin:RAiTAC0BxVafiCDY@cluster0-shard-00-00-z72vv.mongodb.net:27017,cluster0-shard-00-01-z72vv.mongodb.net:27017,cluster0-shard-00-02-z72vv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
-    database: 'mongodb://localhost/aws_test',
+    database:'mongodb://admin:RAiTAC0BxVafiCDY@cluster0-shard-00-00-z72vv.mongodb.net:27017,cluster0-shard-00-01-z72vv.mongodb.net:27017,cluster0-shard-00-02-z72vv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    //database: 'mongodb://localhost/aws_test',
     jwtSecret: 'I Love Erica!!!!',
     jwtExpiration: 1000000
 };
